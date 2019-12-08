918a0fce2cddbb6a08990d4d8ce5db88507fc689
Mark Smith <mark.smith@practicalpoetry.co.uk>
2019-12-08 11:06:54 +0000

    Reformatted Day 1 soluion

--- 2019/day_01/python/part1.py
+++ 2019/day_01/python/part1.py
@@ -3,5 +3,4 @@
 from math import floor
 
 
-print(sum(floor(int(line) / 3.0) - 2 for line in open('input_01.txt', 'r')))
-    
+print(sum(floor(int(line) / 3.0) - 2 for line in open("input_01.txt", "r")))
--- 2019/day_01/python/part2.py
+++ 2019/day_01/python/part2.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
+import argparse
+import logging
 from math import floor
+import sys
 
 
 def calculate_fuel(weight):
-    new_weight = floor(int(weight) / 3.0) - 2
-    if new_weight > 0:
-        return new_weight + calculate_fuel(new_weight)
-    else:
+    if (new_weight := floor(int(weight) / 3.0) - 2) <= 0:
         return 0
+    return new_weight + calculate_fuel(new_weight)
 
-print(sum(calculate_fuel(int(line)) for line in open('input_01.txt', 'r')))
-    
+
+def solve(datafile):
+    return sum(calculate_fuel(int(line)) for line in datafile)
+
+
+def main(argv=sys.argv[1:]):
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--verbose", action="store_true")
+    ap.add_argument("datafile", type=argparse.FileType(mode="r"))
+
+    args = ap.parse_args(argv)
+    print(solve(args.datafile))
+
+
+if __name__ == "__main__":
+    main()
