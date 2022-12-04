f4c3cc3cb5da93bc3428420b6dc59c55c3ad9069
Judy2k <judy@judy.co.uk>
2022-12-04 11:12:59 +0000

    Day 4, Part 2.

---
+++ 2022/day_04/python/part2.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+from logging import debug, info, warn
+import sys
+
+
+def parse_ranges(line):
+    return [tuple(int(s) for s in rs.split("-")) for rs in line.strip().split(",")]
+
+
+def overlaps(ab, cd):
+    a, b = ab
+    c, d = cd
+    return (a <= c and b >= c) or (c <= a and d >= a)
+
+
+def solve(datafile):
+    return sum(overlaps(*parse_ranges(line)) for line in datafile)
+
+
+def main(argv=sys.argv[1:]):
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--verbose", action="store_true")
+    ap.add_argument("datafile", type=argparse.FileType(mode="r"))
+
+    args = ap.parse_args(argv)
+
+    logging.basicConfig(format="%(message)s", level=logging.DEBUG if args.verbose else logging.WARNING)
+    print(solve(args.datafile))
+
+
+def test_sample():
+    assert parse_ranges("2-4,6-8\n") == [(2, 4), (6, 8)]
+    assert overlaps((2, 8), (3, 7)) == True
+    assert overlaps((6, 6), (4, 6)) == True
+    assert overlaps((2, 6), (4, 8)) == True
+    assert list(overlaps(*parse_ranges(line)) for line in open("../sample.txt")) == [
+        False,
+        False,
+        True,
+        True,
+        True,
+        True,
+    ]
+    assert solve(open("../sample.txt")) == 4
+
+
+if __name__ == "__main__":
+    main()
