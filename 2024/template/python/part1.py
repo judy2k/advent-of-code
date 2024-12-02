90d85decc2869311f948fb739bb1827f38229293
Mark Smith <judy@judy.co.uk>
2024-12-02 17:14:51 +0000

    Using last year's template stuff.

---
+++ 2024/template/python/part1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+from logging import debug, info, warn
+from pathlib import Path
+import sys
+
+
+def solve(datafile):
+    for line in datafile:
+        pass
+
+    return 0
+
+
+def main(argv=sys.argv[1:]):
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--verbose", action="store_true")
+    ap.add_argument("datafile", type=argparse.FileType(mode="r"))
+
+    args = ap.parse_args(argv)
+
+    logging.basicConfig(
+        format="%(message)s",
+        level=logging.DEBUG if args.verbose else logging.WARNING,
+    )
+    print(solve(args.datafile))
+
+# Tests ------------------------------------------------------------------------
+
+def test_sample():
+    datafile = Path(__file__).parent.parent.joinpath("sample.txt").open()
+    assert solve(datafile) == TODO
+
+
+# def test_input():
+#     datafile = Path(__file__).parent.parent.joinpath("input.txt").open()
+#     assert solve(datafile) == TODO
+
+
+if __name__ == "__main__":
+    main()
