c0fc39bed3aaa24a619f8657182be1cad7c29712
Mark Smith <judy@judy.co.uk>
2024-12-06 23:23:30 +0000

    Well, this might help with Part 2.

--- 2024/day_04/python/part1.py
+++ 2024/day_04/python/part1.py
@@ -1,61 +1,61 @@
 #!/usr/bin/env python3
 
 import argparse
-import itertools
 import logging
-import re
 import sys
 from pathlib import Path
+
+XMAS = list("XMAS")
 
 
 def solve(datafile):
     tally = 0
-    input_lines = [line.strip() for line in datafile.readlines()]
-    for line in all_lines(input_lines):
-        tally += len(re.findall("XMAS", line))
+    input_lines: list[list[str]] = [list(line.strip()) for line in datafile.readlines()]
+
+    row_count = len(input_lines)
+    col_count = len(input_lines[0])
+
+    for row in range(row_count):
+        for col in range(col_count):
+            if input_lines[row][col] == "X":
+                tally += sum(
+                    1
+                    for slice in (
+                        input_lines[row][col : col + 4],
+                        input_lines[row][col : col - 4 if col >= 4 else None : -1],
+                        [
+                            input_lines[r][col]
+                            for r in range(row, min(row_count, row + 4))
+                        ],
+                        [
+                            input_lines[r][col]
+                            for r in range(row, row - 4 if row >= 4 else -1, -1)
+                        ],
+                        [
+                            input_lines[row + i][col + i]
+                            for i in range(4)
+                            if row + i < row_count and col + i < col_count
+                        ],
+                        [
+                            input_lines[row - i][col - i]
+                            for i in range(4)
+                            if row - i >= 0 and col - i >= 0
+                        ],
+                        [
+                            input_lines[row + i][col - i]
+                            for i in range(4)
+                            if row + i < row_count and col - i >= 0
+                        ],
+                        [
+                            input_lines[row - i][col + i]
+                            for i in range(4)
+                            if row - i >= 0 and col + i < col_count
+                        ],
+                    )
+                    if slice == XMAS
+                )
 
     return tally
-
-
-def all_lines(input_lines):
-    return and_reversed(
-        itertools.chain(
-            [line for line in input_lines],
-            ["".join(col) for col in (zip(*input_lines))],
-            ["".join(col) for col in (diagonal(input_lines))],
-            [
-                "".join(col)
-                for col in reversed(list(diagonal(list(reversed(input_lines)))))
-            ],
-        )
-    )
-
-
-def and_reversed(inputs):
-    for input in inputs:
-        yield input
-        yield "".join(reversed(input))
-
-
-def diagonal(input_lines):
-    line_len = len(input_lines[0])
-    for start_x in range(line_len - 1, 0, -1):
-        line = []
-        x, y = start_x, 0
-        while x < line_len and y < len(input_lines):
-            line.append(input_lines[y][x])
-            x += 1
-            y += 1
-        yield "".join(line)
-
-    for start_y in range(len(input_lines)):
-        line = []
-        x, y = 0, start_y
-        while x < line_len and y < len(input_lines):
-            line.append(input_lines[y][x])
-            x += 1
-            y += 1
-        yield "".join(line)
 
 
 def main(argv=sys.argv[1:]):
@@ -83,53 +83,6 @@ def test_sample():
 def test_input():
     datafile = Path(__file__).parent.parent.joinpath("input.txt").open()
     assert solve(datafile) == 2483
-
-
-def test_all_lines():
-    input_lines = [
-        line.strip()
-        for line in """abc
-def
-ghi""".splitlines()
-    ]
-    assert list(all_lines(input_lines)) == [
-        # Horizontal:
-        "abc",
-        "cba",
-        "def",
-        "fed",
-        "ghi",
-        "ihg",
-        # Vertical:
-        "adg",
-        "gda",
-        "beh",
-        "heb",
-        "cfi",
-        "ifc",
-        # Diagonals, down to the right:
-        "c",
-        "c",
-        "bf",
-        "fb",
-        "aei",
-        "iea",
-        "dh",
-        "hd",
-        "g",
-        "g",
-        # Diagonals, up to the right:
-        "a",
-        "a",
-        "db",
-        "bd",
-        "gec",
-        "ceg",
-        "hf",
-        "fh",
-        "i",
-        "i",
-    ]
 
 
 if __name__ == "__main__":
