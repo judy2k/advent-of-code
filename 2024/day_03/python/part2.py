2088446511621342639123d1591159b6cfdd6ca6
Mark Smith <judy@judy.co.uk>
2024-12-06 22:28:22 +0000

    Minor optimisation for day 3.

--- 2024/day_03/python/part2.py
+++ 2024/day_03/python/part2.py
@@ -6,20 +6,20 @@ import re
 import sys
 from pathlib import Path
 
-INSTRUCTION_FINDER = re.compile(r"""mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)""")
-INSTRUCTION_PARSER = re.compile(r"""mul\((\d{1,3}),(\d{1,3})\)""")
+INSTRUCTION_FINDER = re.compile(r"""mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)""")
 
 
 def solve(datafile):
     active = True
     tally = 0
-    for inst in INSTRUCTION_FINDER.findall(datafile.read()):
-        if inst == "do()":
+    for inst in INSTRUCTION_FINDER.finditer(datafile.read()):
+        s = inst.group()
+        if s == "do()":
             active = True
-        elif inst == "don't()":
+        elif s == "don't()":
             active = False
-        elif active and inst.startswith("mul"):
-            a, b = INSTRUCTION_PARSER.match(inst).groups()
+        elif active and s.startswith("mul"):
+            a, b = inst.groups()
             tally += int(a) * int(b)
 
     return tally
@@ -48,7 +48,7 @@ def test_sample():
 
 
 def test_regex():
-    match = INSTRUCTION_PARSER.match("mul(12,13)")
+    match = INSTRUCTION_FINDER.match("mul(12,13)")
     assert match is not None
     assert match.group(1) == "12"
     assert match.group(2) == "13"
