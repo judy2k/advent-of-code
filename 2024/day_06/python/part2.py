7886ffe1775a5fd1fa8997904409c484e9682d4b
Mark Smith <judy@judy.co.uk>
2024-12-09 10:36:17 +0000

    About 6 times faster now.

--- 2024/day_06/python/part2.py
+++ 2024/day_06/python/part2.py
@@ -3,8 +3,9 @@
 import argparse
 import logging
 import sys
-from itertools import product
 from pathlib import Path
+
+from tqdm import tqdm
 
 DIRECTIONS = [
     (-1, 0),
@@ -12,6 +13,27 @@ DIRECTIONS = [
     (1, 0),
     (0, -1),
 ]
+
+
+def visited_locations(grid, location):
+    direction = (-1, 0)
+    visited_locations = set()
+
+    def inside_grid(row, col):
+        return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+
+    def blocked(row, col):
+        return inside_grid(row, col) and (grid[row][col] == "#")
+
+    while inside_grid(*location):
+        visited_locations.add(location)
+        pending_location = (location[0] + direction[0], location[1] + direction[1])
+        if blocked(*pending_location):
+            direction = DIRECTIONS[(DIRECTIONS.index(direction) + 1) % 4]
+        else:
+            location = pending_location
+
+    return visited_locations
 
 
 def solve_grid(grid, location, blocked_location):
@@ -36,13 +58,13 @@ def solve_grid(grid, location, blocked_location):
         return DIRECTIONS[(DIRECTIONS.index(direction) + 1) % 4]
 
     while inside_grid(*location):
+        if (location, direction) in visited_locations:
+            return True
         visited_locations.add((location, direction))
         pending_location = (location[0] + direction[0], location[1] + direction[1])
         if blocked(*pending_location):
             direction = rotate(direction)
         else:
-            if (pending_location, direction) in visited_locations:
-                return True
             location = pending_location
 
     return False
@@ -50,21 +72,15 @@ def solve_grid(grid, location, blocked_location):
 
 def solve(datafile):
     grid = [list(line.strip()) for line in datafile]
-    grid_height = len(grid)
-    grid_width = len(grid[0])
-
     for i, row in enumerate(grid):
         if "^" in row:
             location = (i, row.index("^"))
             grid[location[0]][location[1]] = "."
             break
-
-    tally = 0
-    for row, col in product(range(grid_height), range(grid_width)):
-        if solve_grid(grid, location, (row, col)):
-            tally += 1
-
-    return tally
+    return sum(
+        solve_grid(grid, location, (row, col))
+        for row, col in tqdm(visited_locations(grid, location) - set((location)))
+    )
 
 
 def main(argv=sys.argv[1:]):
