f87bbaf7a3e651acb9dcd2d05042ee9eac9f3dd2
Mark Smith <judy@judy.co.uk>
2024-12-15 10:42:12 +0000

    Day 10 Solved

---
+++ 2024/day_10/python/part1.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import sys
+from pathlib import Path
+
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+
+def solve(datafile):
+    grid = [[int(c) for c in line.strip()] for line in datafile]
+
+    trailheads = []
+    for row_n, row in enumerate(grid):
+        for col_n, val in enumerate(row):
+            if val == 0:
+                trailheads.append((row_n, col_n))
+
+    return sum(count_peaks(start, grid) for start in trailheads)
+
+
+def count_peaks(pos, grid, peaks=None):
+    if peaks is None:
+        peaks = set()
+    val = grid[pos[0]][pos[1]]
+    if val == 9:
+        peaks.add(pos)
+    else:
+        peaks.union(
+            count_peaks(candidate, grid, peaks)
+            for candidate in [
+                (pos[0] + option[0], pos[1] + option[1])
+                for option in DIRECTIONS
+            ]
+            if 0 <= candidate[0] < len(grid)
+            and 0 <= candidate[1] < len(grid[0])
+            and grid[candidate[0]][candidate[1]] == val + 1
+        )
+
+    return len(peaks)
+
+
+def main(argv=sys.argv[1:]):
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-v", "--verbose", action="store_true")
+    ap.add_argument("datafile", type=argparse.FileType(mode="r"))
+
+    args = ap.parse_args(argv)
+
+    logging.basicConfig(
+        format="%(message)s",
+        level=logging.DEBUG if args.verbose else logging.WARNING,
+    )
+    print(solve(args.datafile))
+
+
+# Tests ------------------------------------------------------------------------
+
+
+def test_sample1():
+    datafile = Path(__file__).parent.parent.joinpath("sample1.txt").open()
+    assert solve(datafile) == 1
+
+
+def test_sample2():
+    datafile = Path(__file__).parent.parent.joinpath("sample2.txt").open()
+    assert solve(datafile) == 36
+
+
+def test_input():
+    datafile = Path(__file__).parent.parent.joinpath("input.txt").open()
+    assert solve(datafile) == 694
+
+
+if __name__ == "__main__":
+    main()
