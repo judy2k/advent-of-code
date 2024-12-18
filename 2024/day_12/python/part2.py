6db3caccb21b47be60232a2a733d5dbc11de6f9d
Mark Smith <judy@judy.co.uk>
2024-12-18 15:43:04 +0000

    Day 12 Part 2 Solved (and then obfuscated)

--- 2024/day_12/python/part2.py
+++ 2024/day_12/python/part2.py
@@ -1,34 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
-import io
 import logging
 import sys
-from itertools import product
+from itertools import chain, groupby, product
 from pathlib import Path
 
-from tqdm import tqdm
-
 DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-PERIMETER_OFFSETS = [
-    # Each pair is starting (offset, direction).
-    # Perimeter always keeps the region to the right-hand side of travel.
-    # Offset is (0, 0) starting at the top-left corner of the plot.
-    # The order of these offsets is the same as the order of DIRECTIONS,
-    # assuming the direction is the direction looking out of the region.
-    # (Perpendicular to the perimeter)
-    [(0, 0), (0, 1)],
-    [(0, 1), (1, 0)],
-    [(1, 1), (0, -1)],
-    [(1, 0), (-1, 0)],
-]
 
 
 class Region:
     def __init__(self, grid, starting_pos):
         self.letter = grid[starting_pos[0]][starting_pos[1]]
         self.plots = set()
-        self.perimeters = {}
         self._add_plot(grid, starting_pos)
 
     def contains(self, plot):
@@ -36,9 +20,7 @@ class Region:
 
     def _add_plot(self, grid, pos):
         self.plots.add(pos)
-        for direction, (p_offset, p_direction) in zip(
-            DIRECTIONS, PERIMETER_OFFSETS
-        ):
+        for direction in DIRECTIONS:
             neighbour = (pos[0] + direction[0], pos[1] + direction[1])
             if (
                 0 <= neighbour[0] < len(grid)
@@ -47,42 +29,64 @@ class Region:
             ):
                 if neighbour not in self.plots:
                     self._add_plot(grid, neighbour)
-            else:
-                self.perimeters[
-                    (pos[0] + p_offset[0], pos[1] + p_offset[1])
-                ] = p_direction
 
-    def p_score(self):
-        print(f"SCORE: {self.letter}")
-        loc, direction = next(iter(self.perimeters.items()))
-        # Find a corner:
-        while True:
-            loc2 = (loc[0] + direction[0], loc[1] + direction[1])
-            direction2 = self.perimeters.get(loc2)
-            if direction2 != direction:
-                break
-            loc, direction = loc2, direction2
+    def bounding_box(self):
+        rows, cols = zip(*self.plots)
+        min_row, max_row = min(rows), max(rows)
+        min_col, max_col = min(cols), max(cols)
+        return min_row, max_row, min_col, max_col
 
-        start_loc = loc
-        length = 0
-        for _ in range(len(self.perimeters)):
-            loc2 = (loc[0] + direction[0], loc[1] + direction[1])
-            direction2 = self.perimeters.get(loc2)
-            print(f"{loc} -> {direction}, {loc2} -> {direction2}")
-            if direction2 != direction:
-                print("   Increment direction")
-                length += 1
-            if loc2 == start_loc:
-                break
-            loc, direction = loc2, direction2
-        else:
-            raise Exception("Looping perimeters!")
-        return length
+    def perimeters(self):
+        min_row, max_row, min_col, max_col = self.bounding_box()
+
+        # Scan down:
+        perimeters = sum(
+            chain(
+                (
+                    len(
+                        [
+                            k
+                            for k, v in groupby(
+                                [
+                                    ((row, col) in self.plots)
+                                    - ((row + 1, col) in self.plots)
+                                    for col in range(min_col - 1, max_col + 2)
+                                ]
+                            )
+                            if k != 0
+                        ]
+                    )
+                    for row in range(min_row - 1, max_row + 1)
+                ),
+                (
+                    len(
+                        [
+                            k
+                            for k, _ in groupby(
+                                [
+                                    ((row, col) in self.plots)
+                                    - (
+                                        (
+                                            row,
+                                            col + 1,
+                                        )
+                                        in self.plots
+                                    )
+                                    for row in range(min_row - 1, max_row + 2)
+                                ]
+                            )
+                            if k != 0
+                        ]
+                    )
+                    for col in range(min_col - 1, max_col + 1)
+                ),
+            )
+        )
+
+        return perimeters
 
     def score(self):
-        p_score = self.p_score()
-        print(f"{self.letter}: {len(self.plots)} x {p_score}")
-        return len(self.plots) * p_score
+        return len(self.plots) * self.perimeters()
 
 
 def solve(datafile):
@@ -97,7 +101,7 @@ def solve(datafile):
         else:
             regions.append(Region(grid, (row, col)))
 
-    return sum(region.score() for region in tqdm(regions))
+    return sum(region.score() for region in regions)
 
 
 def main(argv=sys.argv[1:]):
@@ -117,16 +121,6 @@ def main(argv=sys.argv[1:]):
 # Tests ------------------------------------------------------------------------
 
 
-def test_enclosed():
-    datafile = io.StringIO("""AAAAAA
-AAABBA
-AAABBA
-ABBAAA
-ABBAAA
-AAAAAA""")
-    assert solve(datafile) == 368
-
-
 def test_sample():
     datafile = Path(__file__).parent.parent.joinpath("sample.txt").open()
     assert solve(datafile) == 1206
@@ -134,7 +128,7 @@ def test_sample():
 
 def test_input():
     datafile = Path(__file__).parent.parent.joinpath("input.txt").open()
-    assert solve(datafile) == 1533024
+    assert solve(datafile) == 910066
 
 
 if __name__ == "__main__":
